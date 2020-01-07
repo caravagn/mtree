342ceaf772896c8a94d9dedec98a962d2d636c0e
Giulio Caravagna <gcaravagn@gmail.com>
2020-01-07 01:42:00 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -34,11 +34,19 @@
   
   if (mtree_welcome_message)
   {
-    pio::pioHdr('Mtree - Mutation Trees in cancer')
-    pio::pioStr("Author : ",
-                "Giulio Caravagna <gcaravagn@gmail.com>",
-                suffix = '\n')
-    pio::pioStr("GitHub : ", "caravagn/mtree", suffix = '\n')
+    # pio::pioHdr('Mtree - Mutation Trees in cancer')
+    # pio::pioStr("Author : ",
+    #             "Giulio Caravagna <gcaravagn@gmail.com>",
+    #             suffix = '\n')
+    # pio::pioStr("GitHub : ", "caravagn/mtree", suffix = '\n')
+    
+    pk = 'mtree'
+    pk_l = 'Mutation trees in cancer'
+    www = "https://caravagn.github.io/mtree/"
+    em = "gcaravagn@gmail.com"
+    
+    cli::cli_alert_success(
+      'Loading {.field {pk}}, {.emph \'{pk_l}\'}. Support : {.url { www}}' )
     
     options(mtree_welcome_message = FALSE)
   }
